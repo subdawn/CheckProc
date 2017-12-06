2bd0de52f61ce8bf47b3608625492a037a413889
yi yang <542741629@qq.com>
2017-12-06 22:07:04 +0800

    new file:   src/CheckProc.pro 	new file:   src/CheckProc.pro.user 	new file:   src/main.cpp 	new file:   src/task.cpp 	new file:   src/task.h

---
+++ src/task.h
@@ -0,0 +1,31 @@
+#ifndef TASK_H
+#define TASK_H
+
+
+class Task
+{
+public:
+    Task();
+private:
+    /* task specification:
+     * 1.dir
+     * 2.cmd
+     * 3.parameterlist
+     * 4.isBackGroundRunning
+     */
+
+    /* task statics:
+     * 1.start time
+     * 2.stop time
+     * 3.stop reason
+     */
+
+    /* task running：
+     * 1.pid:
+     * 2.duration:
+     * 3.cpu
+     * 4.mem
+     */
+};
+
+#endif // TASK_H
